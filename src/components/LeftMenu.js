b94e14a7c935a1fe622321b3dcb17adb6b719c20
Mauricio Lemus <rodeznomauricio@gmail.com>
2022-07-02 18:55:28 -0600

    Initial configuration

---
+++ src/components/LeftMenu.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function LeftMenu() {
+  return <div className='h-screen w-56 bg-black'>LeftMenu</div>;
+}
---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import LeftMenu from '../components/LeftMenu';
+
+const Home = () => {
+  return (
+    <div className='w-screen h-screen overflow-hidden bg-[#1d1d1d]'>
+      <LeftMenu />
+    </div>
+  );
+};
+
+export default Home;
