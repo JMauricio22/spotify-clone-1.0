ebbd967b54d70e8b75814e776f6cfb33de0e82a3
Mauricio Lemus <rodeznomauricio@gmail.com>
2022-07-11 11:47:03 -0600

    Restructuring pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,57 @@
-import Home from '../components/Home';
+import React, { useEffect } from 'react';
+import { useSession } from 'next-auth/react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchRecommendations } from '../features/recommendations';
+import useSpotify from '../hooks/useSpotify';
+import Container from '../components/Container';
+import { generateItemsWithRecommendationAdaptor, generateItemsWithArtisAdapter } from '../utils/cardItemAdapter';
+import CardSection from '../components/CardSection';
+import CardContainer from '../components/CardContainer';
+import ArtistCard from '../components/ArtistCard';
+import ArtistMobileCard from '../components/ArtistMobileCard';
+import Loader from '../components/Loader';
 
-const Index = () => {
-  return <Home />;
+const ItemList = CardSection(CardContainer);
+
+const Home = () => {
+  const { data: session } = useSession();
+  const spotifyApi = useSpotify();
+  const dispatch = useDispatch();
+  const loading = useSelector((state) => state.recommendations.loading);
+  const recommendations = useSelector((state) => state.recommendations.items);
+  const artists = useSelector((state) => state.myTopArtists.items);
+
+  useEffect(() => {
+    if (recommendations.length === 0 && spotifyApi.getAccessToken()) {
+      dispatch(fetchRecommendations());
+    }
+  }, [session, spotifyApi]);
+
+  return (
+    <Container>
+      <section className='pb-10'>
+        {loading && <Loader />}
+        {recommendations.length > 0 && (
+          <ItemList
+            title='Recommendations'
+            items={generateItemsWithRecommendationAdaptor(recommendations)}
+            card={(props) => <ArtistCard {...props} />}
+            cardMobile={(props) => <ArtistMobileCard {...props} />}
+          />
+        )}
+        {artists.length > 0 && (
+          <ItemList
+            title='Your favorite artists'
+            items={generateItemsWithArtisAdapter(artists)}
+            card={(props) => <ArtistCard rounded {...props} />}
+            cardMobile={(props) => <ArtistMobileCard rounded {...props} />}
+          />
+        )}
+      </section>
+    </Container>
+  );
 };
 
-Index.layout = true;
+Home.layout = true;
 
-export default Index;
+export default Home;
--- src/pages/search.js
+++ src/pages/search.js
@@ -1,10 +1,128 @@
-import React from 'react';
-import Search from '../components/Search';
+import React, { useEffect, useRef, useState } from 'react';
+import { SearchIcon, XIcon } from '@heroicons/react/solid';
+import Container from '../components/Container';
+import { searchItems, clearSearch, setFilter, setQuery } from '../features/search';
+import { useDispatch, useSelector } from 'react-redux';
+import ArtistCard from '../components/ArtistCard';
+import CardItemList from '../components/CardSection';
+import { generateItemsWithArtisAdapter, generateItemsWithPlaylistAdapter } from '../utils/cardItemAdapter';
+import CardContainer from '../components/CardContainer';
+import ArtistMobileCard from '../components/ArtistMobileCard';
+import ArtistMobileSearch from '../components/ArtistMobileSearch';
+import VerticalCardList from '../components/VerticalCardList';
+import ColumnsCardList from '../components/ColumnsCardList';
+import { Transition } from '@headlessui/react';
+import Loader from '../components/Loader';
 
-const SearchPage = () => {
-  return <Search />;
+const filters = ['All', 'Artist', 'Playlist'];
+
+const ItemList = CardItemList(CardContainer);
+
+const Search = () => {
+  const dispatch = useDispatch();
+  const items = useSelector((state) => state.search.items);
+  const loading = useSelector((state) => state.search.loading);
+  const currentFilter = useSelector((state) => state.search.filter);
+  const query = useSelector((state) => state.search.query);
+  const [showCloseButton, setShowCloseButton] = useState(false);
+  const [searchItemsPromise, setSearchItemsPromise] = useState(null);
+  const timeoutId = useRef(null);
+
+  /* When the component is unmounted, cancel the current request and reset the search state */
+  useEffect(() => {
+    return () => {
+      if (searchItemsPromise) {
+        searchItemsPromise.abort();
+      }
+      dispatch(clearSearch());
+    };
+  }, []);
+
+  const search = ({ target }) => {
+    /* If timeoutId exists then stop with clearTimeout */
+    if (timeoutId.current) {
+      clearTimeout(timeoutId.current);
+    }
+    /* Show close button */
+    setShowCloseButton(!!target.value);
+    /* Set query */
+    dispatch(setQuery(target.value));
+    /* Delay Search 300ms*/
+    setTimeout(() => {
+      const promise = dispatch(searchItems({ query: target.value }));
+      setSearchItemsPromise(promise);
+    }, 300);
+  };
+
+  const cancelSearch = () => {
+    /* If search promise exists then cancel current request */
+    if (searchItemsPromise) {
+      searchItemsPromise.abort();
+    }
+    /* Hidden close button */
+    setShowCloseButton(false);
+    /* Reset state to initial state */
+    dispatch(clearSearch());
+  };
+
+  return (
+    <Container>
+      <div className='bg-black h-[60px] flex justify-start items-center pl-8'>
+        <div className='bg-gray-100 outline-none px-3 rounded-full text-black flex items-center space-x-3'>
+          <span>
+            <SearchIcon className='w-5 h-5' />
+          </span>
+          <input
+            value={query}
+            onChange={search}
+            className='bg-transparent w-48 placeholder:text-gray-400 py-2 pr-4 outline-none placeholder:font-gothambook placeholder:text-sm'
+            placeholder='Artist or songs'
+          />
+          <button className='w-5 h-5' disabled={!query} onClick={cancelSearch}>
+            <Transition appear={true} show={showCloseButton}>
+              <XIcon className='w-full h-full' />
+            </Transition>
+          </button>
+        </div>
+      </div>
+      {loading && <Loader />}
+      {!!(!loading && items) && (
+        <div className='mt-4 pl-10 space-x-2'>
+          {filters.map((filter) => (
+            <span
+              className={`inline-block px-5 py-2 text-sm rounded-full font-gothammedium cursor-pointer ${
+                filter.toLowerCase() === currentFilter ? 'bg-slate-100 text-[#121212]' : 'text-slate-100 bg-[#121212] '
+              }`}
+              key={`filter-item-${filter}`}
+              onClick={() => dispatch(setFilter(filter.toLowerCase()))}
+            >
+              {filter}
+            </span>
+          ))}
+        </div>
+      )}
+      {!loading && (currentFilter === 'all' || currentFilter === 'artist') && items?.artists?.items?.length > 0 && (
+        <ItemList
+          title='Artists'
+          layout={(items) => <VerticalCardList items={items} />}
+          items={generateItemsWithArtisAdapter(items.artists.items)}
+          card={(props) => <ArtistCard rounded {...props} />}
+          cardMobile={(props) => <ArtistMobileSearch rounded {...props} />}
+        />
+      )}
+      {!loading && (currentFilter === 'all' || currentFilter === 'playlist') && items?.playlists?.items?.length > 0 && (
+        <ItemList
+          title='Playlist'
+          layout={(items) => <ColumnsCardList items={items} />}
+          items={generateItemsWithPlaylistAdapter(items.playlists.items)}
+          card={(props) => <ArtistCard {...props} />}
+          cardMobile={(props) => <ArtistMobileCard {...props} />}
+        />
+      )}
+    </Container>
+  );
 };
 
-SearchPage.layout = true;
+Search.layout = true;
 
-export default SearchPage;
+export default Search;
