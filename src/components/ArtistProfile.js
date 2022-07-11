0bbaeda93fd7d33b596246ae41f65f159bf0b9e2
Mauricio Lemus <rodeznomauricio@gmail.com>
2022-07-11 00:02:39 -0600

    Add loader component in artist profile

--- src/components/ArtistProfile.js
+++ src/components/ArtistProfile.js
@@ -10,12 +10,14 @@ import Image from 'next/image';
 import VerifiedIcon from '../assets/icons/verified.svg';
 import PlaylistWith3Cols from './PlaylistWith3Cols';
 import { convertTrackItemsToSongItems } from '../utils/songItemAdapter';
+import Loader from './Loader';
 
 export default function Artistartist() {
   const { data: session } = useSession();
   const spotifyApi = useSpotify();
   const dispatch = useDispatch();
   const artist = useSelector((state) => state.selectedArtist.data);
+  const loading = useSelector((state) => state.selectedArtist.loading);
   const { query } = useRouter();
 
   useEffect(() => {
@@ -26,30 +28,33 @@ export default function Artistartist() {
 
   return (
     <Container>
-      {artist && (
-        <>
-          <Hero
-            imageUrl={artist.images[0].url}
-            title={artist.name}
-            style={{ background: 'linear-gradient(61deg, rgb(25 25 30) 0%, rgb(32 37 39  ) 70%)' }}
-            beforeTitle={
-              <p className='flex items-center mb-2'>
-                <Image src={VerifiedIcon} width={25} height={25} layout='fixed' />
-                <span className='ml-1 text-md'>Verified Artist</span>
-              </p>
-            }
-            afterTitle={
-              <div>
-                <p className='font-gothammedium text-md'>
-                  Followers:{' '}
-                  <span className='text-gray-300 text-sm'> {artist.followers.total.toLocaleString('es-US')} </span>{' '}
+      <>
+        {loading && <Loader />}
+        {!loading && artist && (
+          <>
+            <Hero
+              imageUrl={artist.images[0].url}
+              title={artist.name}
+              style={{ background: 'linear-gradient(61deg, rgb(25 25 30) 0%, rgb(32 37 39  ) 70%)' }}
+              beforeTitle={
+                <p className='flex items-center mb-2'>
+                  <Image src={VerifiedIcon} width={25} height={25} layout='fixed' />
+                  <span className='ml-1 text-md'>Verified Artist</span>
                 </p>
-              </div>
-            }
-          />
-          <PlaylistWith3Cols items={convertTrackItemsToSongItems(artist.tracks)} />
-        </>
-      )}
+              }
+              afterTitle={
+                <div>
+                  <p className='font-gothammedium text-md'>
+                    Followers:{' '}
+                    <span className='text-gray-300 text-sm'> {artist.followers.total.toLocaleString('es-US')} </span>{' '}
+                  </p>
+                </div>
+              }
+            />
+            <PlaylistWith3Cols items={convertTrackItemsToSongItems(artist.tracks)} />
+          </>
+        )}
+      </>
     </Container>
   );
 }
