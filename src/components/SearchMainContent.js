d4150b3fb8f430da0b1341b97dfa2da5d8579ba6
Mauricio Lemus <rodeznomauricio@gmail.com>
2022-10-02 20:09:43 -0600

    Reproduce main result on search page

--- src/components/SearchMainContent.js
+++ src/components/SearchMainContent.js
@@ -1,13 +1,19 @@
 import React, { useId, useState } from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { play } from '../features/player';
 import { selectSearchItems } from '../features/search';
 import PlayButton from './PlayButton';
 import SongSmallItem from './SongSmallItem';
 
 export default function SearchMainContent() {
+  const dispatch = useDispatch();
   const [showPlayButton, setShowPlayButton] = useState(false);
   const id = useId();
   const items = useSelector(selectSearchItems);
+
+  const uri = items?.artists.items[0].uri;
+
+  const playAllTracks = () => dispatch(play(uri));
 
   return (
     <section className='mt-10 w-[95%] mx-auto pl-4 grid-rows-1 4lg:grid-cols-[450px_1fr] lg:grid-cols-2 grid-cols-1 xl:gap-8 max-w-[95%] lg:gap-4 md:grid hidden'>
@@ -28,7 +34,11 @@ export default function SearchMainContent() {
           </p>
           <span className='uppercase py-2 px-4 text-xs font-gothammedium bg-[#131313] rounded-full'>Artist</span>
         </div>
-        <PlayButton show={showPlayButton} className='absolute bottom-6 right-6 z-30 hidden lg:block' />
+        <PlayButton
+          show={showPlayButton}
+          playAllTracks={playAllTracks}
+          className='absolute bottom-6 right-6 z-30 hidden lg:block'
+        />
       </article>
       <article className='lg:block hidden'>
         <h2 className='font-gothammedium text-xl mb-3 lg:mt-0 mt-3'>Songs</h2>
